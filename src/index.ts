f071304feb708dd33982e32c44c24fb853583bd7
Alejandro Parra <alejandro.parra@gmail.com>
2020-05-15 16:33:59 -0300

    cambios de doc

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+//for test values globals inside module
 let m = '';
+//fnc
 const greeter = (name: string) => {
   m = name;
   return `Hello ${m}`;
 };
+//fnc
 const dissmiser = () => `Bye ${m}`;
 export { greeter, dissmiser };
